24f90cb5cbd294aa8b1f04f4e4f4c5f331527996
23eerick <erickwelberth1@hotmail.com>
2023-08-06 00:11:16 -0300

    structure: structure of the unit test model

---
+++ src/tests/user.test.js
@@ -0,0 +1,19 @@
+describe('testing user controllers', () => {
+
+  it('show all users', () => {
+
+  })
+
+  it('create new users', () => {
+
+  })
+
+  it('update existing user', () => {
+
+  })
+
+  it('delete existing user', () => {
+
+  })
+})
+
