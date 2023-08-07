ba729984e0fd230d158d2a2e85d7bb049b375aae
23eerick <erickwelberth1@hotmail.com>
2023-08-07 10:57:43 -0300

    fix: Conflicts in performing unit tests

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { database } from "./config.js";
+import { User } from "./src/models/user.js";
+import { routes } from "./src/routes/routes.js";
+import { sequelize } from "./src/database/db.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+async function startDatabase() {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    await User.sync()
+    console.log(`Database ${database.connect.database} is connected`)
+  } catch (err) {
+    console.error(`Database is not connected, ${err}`)
+  }
+}
+
+export {
+  app,
+  startDatabase
+}
--- index.js
+++ index.js
@@ -1,44 +1,9 @@
-import express from "express";
-import { api, database } from "./config.js";
-import { routes } from "./src/routes/routes.js";
-import { sequelize } from "./src/database/db.js";
-import { User } from "./src/models/user.js";
+import { api } from "./config.js";
+import { app, startDatabase } from "./app.js";
 
-class App {
-  constructor() {
-    this.app = express();
-    this.app.use(express.json());
-    this.connectDatabase = sequelize;
-  }
+const port = api.api.port
 
-  startServer(port) {
-    this.app.listen(port, () => {
-      console.log(`Server started on port ${port}`);
-    })
-  }
-
-  routes() {
-    this.app.use(routes)
-  }
-
-  async database() {
-    try {
-      await this.connectDatabase.authenticate()
-      await this.connectDatabase.sync()
-      await User.sync()
-      console.log(`Database ${database.connect.database} is connected`)
-    } catch (err) {
-      console.error(`Database is not connected, ${err}`)
-    }
-  }
-}
-
-const backend = new App();
-const port = api.api.port;
-
-backend.routes();
-backend.database();
-backend.startServer(port);
-
-const serverApp = backend.app;
-export { serverApp }
+app.listen(port, async () => {
+  await startDatabase();
+  console.log(`Server started on port ${port}`)
+})
