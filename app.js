d5599ee66b462d4d6f33e44f46ce93e012d08d7a
23eerick <erickwelberth1@hotmail.com>
2023-08-11 13:51:16 -0300

    feat: Improved connection to the front

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import { database } from "./config.js";
 import { User } from "./src/models/user.js";
@@ -7,6 +8,7 @@ import { sequelize } from "./src/database/db.js";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 async function startDatabase() {
